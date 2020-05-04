90b5772b78c2a78b82fe29f1cdc1faa0229ad411
Ricardo <ricardo.vejarano.garcia@gmail.com>
2020-05-04 14:30:59 -0500

    improvements in in imports

--- src/app/core/services/storage.service.ts
+++ src/app/core/services/storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import * as CryptoJS from 'crypto-js';
- 
+const enc = require('crypto-js/enc-utf8');
+const sha256 = require('crypto-js/sha256')
+const AES = require("crypto-js/aes");
 const SecureStorage = require('secure-web-storage');
 const SECRET_KEY = 'Ber1g0';
 
@@ -11,21 +12,21 @@ export class StorageService {
   constructor() { }
   public secureStorage = new SecureStorage(sessionStorage, {
     hash: function hash(key) {
-      key = CryptoJS.SHA256(key, SECRET_KEY);
+      key = sha256(key, SECRET_KEY);
  
       return key.toString();
     },
     encrypt: function encrypt(data) {
-      data = CryptoJS.AES.encrypt(data, SECRET_KEY);
+      data = AES.encrypt(data, SECRET_KEY);
  
       data = data.toString();
  
       return data;
     },
     decrypt: function decrypt(data) {
-      data = CryptoJS.AES.decrypt(data, SECRET_KEY);
+      data = AES.decrypt(data, SECRET_KEY);
  
-      data = data.toString(CryptoJS.enc.Utf8);
+      data = data.toString(enc);
  
       return data;
     }
